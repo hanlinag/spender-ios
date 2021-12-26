d6fefdcb42d9e84fcaf5cc043bb5925177750e74
saihanlinaung <45898778+hanlinag@users.noreply.github.com>
2021-12-26 11:49:53 +0630

    table view cell frame

---
+++ Spender/XIBs/TransactionTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TransactionTableViewCell.swift
+//  Spender
+//
+//  Created by Tyler on 25/12/2021.
+//
+
+import UIKit
+
+class TransactionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
