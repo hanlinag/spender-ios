9d7393fe524d4311ec18a6052300855a0a2e8e68
saihanlinaung <45898778+hanlinag@users.noreply.github.com>
2022-08-21 23:29:57 +0630

    aifnup

--- Spender/Controllers/Authentication/Signup/Signup.swift
+++ Spender/Controllers/Authentication/Signup/Signup.swift
@@ -20,28 +20,36 @@ class Signup: SpenderViewController {
         
         tableView.delegate   = self
         tableView.dataSource = self
+        tableView.register(SignupHeaderView.nib(), forHeaderFooterViewReuseIdentifier: SignupHeaderView.identifier)
         
     }
 }
 
 extension Signup: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return vm.signupTableCells.count
+        return vm.signupTableCells.count 
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: signupTableCell, for: indexPath)
         
-        let imgLogo = UIImageView(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
+        let imgLogo = UIImageView(frame: CGRect(x: 0, y: 0, width: (self.view.frame.size.height * 0.1 ), height: (self.view.frame.size.height * 0.1 )))
         imgLogo.image = UIImage(named: "logo-text-orange")
         //imgLogo.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.1).isActive = true
         cell.contentView.addSubview(imgLogo)
-        //cell.contentView = imgLogo
+        
         
         return cell
     }
     
-    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let header = tableView.dequeueReusableHeaderFooterView(withIdentifier: SignupHeaderView.identifier) as! SignupHeaderView
+       
+        header.configure(title: "title.create_account", description: "desc.create_account")
+        header.frame.size.height = self.view.frame.height * 0.25
+        
+        return header
+    }
     
     
 }
---
+++ Spender/XIBs/New Group/SignupCells/SignupTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SignupTableViewCell.swift
+//  Spender
+//
+//  Created by Tyler on 21/08/2022.
+//  Copyright © 2022 Tyler. All rights reserved.
+//
+
+import UIKit
+
+class SignupTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
