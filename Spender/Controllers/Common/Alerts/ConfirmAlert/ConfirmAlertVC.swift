67f02d4ad6636745676d65166f8a8fb6dbd3a06d
saihanlinaung <45898778+hanlinag@users.noreply.github.com>
2022-08-30 23:03:13 +0630

    Add Confirm Alert

---
+++ Spender/Controllers/Common/Alerts/ConfirmAlert/ConfirmAlertVC.swift
@@ -0,0 +1,22 @@
+//
+//  ConfirmAlert.swift
+//  Spender
+//
+//  Created by Tyler on 14/08/2022.
+//  Copyright © 2022 Tyler. All rights reserved.
+//
+
+import UIKit
+
+class ConfirmAlertVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+
+
+}
